fdfe3d6af0b1706a7054cb78904ba28c9b074e2f
sidd0103 <sidbiyer@gmail.com>
2018-01-11 09:01:52 -0800

    hotifx: nav bar fixed

--- public/js/links.js
+++ public/js/links.js
@@ -1,4 +1,6 @@
-$('.link').on('click',function(){
+$('.link').click(function(){
+    console.log("hi");
     var href = $(this).attr('data-to');
+    console.log(href);
     document.location.href = href;
 });
